b99418ff6f6853a83afa95b0ede5bfbbf37805f1
Nikolas Freitas <nikolasbvf@gmail.com>
2019-11-07 00:06:46 -0300

    :lipstick: Use animations into change colors component

--- src/ColorChange/index.js
+++ src/ColorChange/index.js
@@ -4,11 +4,25 @@ import { MudarCor } from "./objectHelper";
 
 const ChangeColor = () => {
   const [cor, novaCor] = useState("#ffffff");
+  const [isClicked, setIsClicked] = useState(false);
 
   return (
-    <Style.Container style={{backgroundColor:cor}}>
+    <Style.Container background={cor}>
       <Style.Titulo>Olha essas Cores!</Style.Titulo>
-      <Style.Button onClick={() => novaCor(MudarCor())}>Clicke - Me</Style.Button>
+      <Style.Button onClick={() => {
+        novaCor(MudarCor());
+        setIsClicked(true);
+        }}>
+        Clicke - Me
+      </Style.Button>
+      {isClicked && (
+        <Style.GrowContainer>
+          <p>O tempo não para</p>
+          <p>
+            <b> timestamp: {Date.now().toString()}</b>
+          </p>
+        </Style.GrowContainer>
+      )}
     </Style.Container>
   );
 };
