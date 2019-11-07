1ddc04e82566f68473ebdb39436e604f6d6dc444
Nikolas Freitas <nikolasbvf@gmail.com>
2019-11-07 00:47:14 -0300

    Updates

---
+++ precache-manifest.561d764d913fd9fa6d9cc8c212f532e9.js
@@ -0,0 +1,43 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62113f27ab0f18a9a81c16783caa76bd",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.9c840e163cc65c831ff0.bundle.js"
+  },
+  {
+    "revision": "09447a8d30aeace958d3",
+    "url": "main.css"
+  },
+  {
+    "url": "runtime~main.9c840e163cc65c831ff0.bundle.js"
+  },
+  {
+    "revision": "2ecb076cb519cc387ede6198e52082ba",
+    "url": "static/media/blond-man.2ecb076c.svg"
+  },
+  {
+    "revision": "e5df74d31ac7a7a2434a4377872981da",
+    "url": "static/media/boy-2.e5df74d3.svg"
+  },
+  {
+    "revision": "c54276906c5e05fd5c0b821a7eb657b2",
+    "url": "static/media/boy.c5427690.svg"
+  },
+  {
+    "revision": "67595189a784d05a60f85f7f85765a47",
+    "url": "static/media/man-2.67595189.svg"
+  },
+  {
+    "revision": "378c61cb8b2f5af0b6ca1309b29538c7",
+    "url": "static/media/man.378c61cb.svg"
+  },
+  {
+    "url": "vendors~main.9c840e163cc65c831ff0.bundle.js"
+  },
+  {
+    "revision": "8e82c18d2983eb0be11c",
+    "url": "vendors~main.css"
+  }
+]);
